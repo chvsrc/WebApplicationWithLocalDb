fc7705aa7f9922bc4337f469c2903d57d243172d
chvsrc <89882229+chvsrc@users.noreply.github.com>
2023-10-10 01:18:10 -0400

    Update Program.cs

--- AspMvcWebApplication/Program.cs
+++ AspMvcWebApplication/Program.cs
@@ -1,7 +1,11 @@
+using AspMvcWebApplication.DatabaseContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//// builder.Services.AddDbContext<EmployeeDbContext>();
 
 var app = builder.Build();
 
