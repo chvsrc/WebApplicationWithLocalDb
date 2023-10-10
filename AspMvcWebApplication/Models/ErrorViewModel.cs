56eb5efeafb500a7df53f0cfc506ba74c3b226f4
chvsrc <chvsrc@gmail.com>
2023-10-09 23:06:21 -0400

    Added Asp Mvc Web Application

---
+++ AspMvcWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspMvcWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
