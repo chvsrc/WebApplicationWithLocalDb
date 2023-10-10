95698b6e61aa3a175c084043f8c30c071f2d4177
chvsrc <chvsrc@gmail.com>
2023-10-09 23:54:44 -0400

    Added Employee DbContext

---
+++ AspMvcWebApplication/DatabaseContext/EmployeeDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AspMvcWebApplication.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspMvcWebApplication.DatabaseContext
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ AspMvcWebApplication/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspMvcWebApplication.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string? Email { get; set; }
+
+    }
+}
